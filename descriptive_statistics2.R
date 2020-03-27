fa50cea00ac8f2bd50bdf98d78026a611895abb0
ameympatil <62697759+ameympatil@users.noreply.github.com>
2020-03-27 13:56:01 +0530

    Add files via upload

---
+++ descriptive_statistics2.R
@@ -0,0 +1,56 @@
+library(datasets)
+data("quakes")
+
+#quakes = read.csv('path/quakes.csv',header=TRUE,sep=",")
+
+earth_quakes <- datasets::quakes
+print(earth_quakes[,c(3,4,5)])
+head(earth_quakes[,c(3,4,5)])
+tail(earth_quakes[,c(3,4,5)])
+earth_quakes$depth
+####################
+summary(earth_quakes$depth)
+summary(earth_quakes[,c(3,4,5)])
+
+#plot and lines
+plot(earth_quakes$depth)
+
+plot(earth_quakes$depth,earth_quakes$mag)
+
+plot(earth_quakes)
+
+plot(earth_quakes$depth , type="l")# p:points, l:line,b:both
+
+plot(earth_quakes$depth, xlab = 'depth concentration', 
+          ylab = 'No of Insances',main= 'depth levels',col='blue')
+
+#Horizontal bar plot
+barplot(earth_quakes$depth , main='Depth concentration',xlab = 'depth', col = "orange",horiz = F)
+
+#Histogram
+hist(earth_quakes$depth)
+hist(earth_quakes$depth, main='Earth Quakes',xlab='depth',col = 'red')
+
+#Single box plot
+ boxplot(earth_quakes$depth)
+
+ #Multiple box plot
+ boxplot(earth_quakes[,3:5],
+         main='Multiple')
+ 
+ #margin of grid(mar)
+ #no of rows and columns(mfrow)
+ #border included or not(bty)
+ #las:1 = horizontal
+ #las:0 = vertical
+ 
+ par(mfrow=c(3,3),mar=c(2,5,2,1),las=0,bty="n")
+ plot(earth_quakes$depth)
+ plot(earth_quakes$depth, earth_quakes$stations)
+ plot(earth_quakes$depth, type= "l")
+ plot(earth_quakes$depth, type= "l")
+ plot(earth_quakes$depth, type= "l")
+ barplot(earth_quakes$depth, main = 'Earthquakes',
+         xlab = 'depth levels', col='red',horiz = T)
+ hist(earth_quakes$depth)
+ boxplot(earth_quakes[,3:5], main='Multiple Box plots')
