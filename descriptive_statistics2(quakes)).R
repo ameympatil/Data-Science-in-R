450757cd0c2cf1d7bf8e0486eeb3458f6faa7178
ameympatil <62697759+ameympatil@users.noreply.github.com>
2020-03-27 14:36:36 +0530

    Update and rename descriptive_statistics2.R to descriptive_statistics2(quakes)).R

--- descriptive_statistics2(quakes)).R
+++ descriptive_statistics2(quakes)).R
@@ -1,28 +1,16 @@
 library(datasets)
 data("quakes")
-
-#quakes = read.csv('path/quakes.csv',header=TRUE,sep=",")
-
 earth_quakes <- datasets::quakes
 print(earth_quakes[,c(3,4,5)])
 head(earth_quakes[,c(3,4,5)])
 tail(earth_quakes[,c(3,4,5)])
-earth_quakes$depth
-####################
 summary(earth_quakes$depth)
 summary(earth_quakes[,c(3,4,5)])
 
-#plot and lines
 plot(earth_quakes$depth)
-
 plot(earth_quakes$depth,earth_quakes$mag)
-
-plot(earth_quakes)
-
-plot(earth_quakes$depth , type="l")# p:points, l:line,b:both
-
-plot(earth_quakes$depth, xlab = 'depth concentration', 
-          ylab = 'No of Insances',main= 'depth levels',col='blue')
+plot(earth_quakes$depth , type="l")
+plot(earth_quakes$depth, xlab = 'depth concentration', ylab = 'No of Insances',main= 'depth levels',col='blue')
 
 #Horizontal bar plot
 barplot(earth_quakes$depth , main='Depth concentration',xlab = 'depth', col = "orange",horiz = F)
@@ -47,8 +35,6 @@ hist(earth_quakes$depth, main='Earth Quakes',xlab='depth',col = 'red')
  par(mfrow=c(3,3),mar=c(2,5,2,1),las=0,bty="n")
  plot(earth_quakes$depth)
  plot(earth_quakes$depth, earth_quakes$stations)
- plot(earth_quakes$depth, type= "l")
- plot(earth_quakes$depth, type= "l")
  plot(earth_quakes$depth, type= "l")
  barplot(earth_quakes$depth, main = 'Earthquakes',
          xlab = 'depth levels', col='red',horiz = T)
